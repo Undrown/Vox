d151cb8c143499bbf21d92a9b0ff90a97ea9288a
undrown.off@gmail.com <undrown.off@gmail.com>
2019-06-27 18:43:35 +0300

    short buffer now works -> IT MUST BE DIVIDABLE BY 2!!!!!

--- app/src/main/java/com/undrown/vox/MainActivity.kt
+++ app/src/main/java/com/undrown/vox/MainActivity.kt
@@ -5,6 +5,7 @@ import android.media.AudioManager
 import android.media.AudioTrack
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.MotionEvent
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,11 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val player = VoxTrack(0f)
+        Thread(player).start()
         mainPanel.setOnTouchListener {_, event ->
-            Toast.makeText(applicationContext,"${event.x*20}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext,"${event.x}", Toast.LENGTH_SHORT).show()
             when(event.action){
                 MotionEvent.ACTION_DOWN -> {
-                    Thread(VoxTrack(event.x*20)).start()
+                    player.setFreqHz(event.x)
+                    player.generateTone()
                 }
             }
             false
@@ -26,64 +30,49 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //private fun generateTone(freqHz: Float, track: AudioTrack) {
-    //    //val count = (44100.0 * 2.0 * (100 / 1000.0)).toInt() and 1.inv()
-    //    val samples = ShortArray(count)
-    //    var i = 0
-    //    while (i < count) {
-    //        val sample = (sin(2.0 * Math.PI * i.toDouble() / (44100.0 / freqHz))*Short.MAX_VALUE).toShort()
-    //        samples[i + 0] = sample
-    //        samples[i + 1] = sample
-    //        i += 2
-    //    }
-    //    track.write(samples, 0, count)
-    //}
-//
-    //private fun generateTrack(count : Int):AudioTrack{
-    //    return AudioTrack(
-    //        AudioManager.STREAM_MUSIC, 44100,
-    //        AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-    //        count * (Short.SIZE / 8), AudioTrack.MODE_STREAM
-    //    )
-    //}
-
-    class VoxTrack(private val freqHz: Float):Runnable{
+    class VoxTrack(private var freqHz: Float):Runnable{
 
         var amplitude:Float = 1.0f
-        val count = 44100
+        val count = 800
+        val countFull = 1000
         val track = generateTrack(count)
 
         private fun generateTrack(count : Int):AudioTrack{
             return AudioTrack(
                 AudioManager.STREAM_MUSIC, 44100,
                 AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-                44100 * (Short.SIZE_BYTES), AudioTrack.MODE_STREAM
+                countFull * (Short.SIZE_BYTES), AudioTrack.MODE_STREAM
             )
         }
 
-        private fun generateTone(freqHz: Float, track: AudioTrack) {
+        public fun generateTone() {
             //val count = (44100.0 * 2.0 * (100 / 1000.0)).toInt() and 1.inv()
-            val samples = ShortArray(count)
+            val samples = ShortArray(countFull)
             var i = 0
             while (i < count) {
                 val sample = (sin(2.0 * Math.PI * i.toDouble() / (44100.0 / freqHz))*Short.MAX_VALUE*amplitude).toShort()
                 samples[i + 0] = sample
                 samples[i + 1] = sample
                 i += 2
-                if(amplitude > 0)
-                    fade()
             }
-            track.write(samples, 0, count)
+            while(i < countFull){
+                samples[i + 0] = (0).toShort()
+                samples[i + 1] = (0).toShort()
+                i += 2
+            }
+            track.write(samples, 0, countFull)
         }
 
         private fun fade(){
             amplitude -= 1/count
         }
 
+        public fun setFreqHz(freq: Float){
+            freqHz = freq
+        }
+
         override fun run() {
-            generateTone(freqHz, track)
             track.play()
-            //fade()
         }
     }
 }
